8ac72c35221a2f306977ec29a5126665330f9a10
Abylay Aiyp <ablyaip@gmail.com>
2024-09-12 23:36:26 -0700

    feat: array as an element

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ function createTextElement(text) {
 }
 
 function render(element, container) {
+  if (Array.isArray(element)) {
+    for (let i = 0; i < element.length; i++) {
+      render(element[i], container);
+    }
+    return;
+  }
   let dom =
     element.type === "TEXT"
       ? document.createTextNode("")
@@ -50,16 +56,32 @@ const OReact = {
 const rootDiv = document.getElementById("root");
 
 /** @jsx OReact.createElement */
-const element = (
-  <div style="background: red">
-    <h1>YOYO</h1>
-    <h2 style="text-align:right">from OReact</h2>
-  </div>
-);
+// const element = (
+//   <div style="background: red">
+//     <h1>YOYO</h1>
+//     <h2 style="text-align:right">from OReact</h2>
+//   </div>
+// );
 
+/** @jsx OReact.createElement */
+function MyApp() {
+  const items = [];
+  for (let i = 0; i < 10; i++) {
+    items.push(<div>my name is {i}</div>);
+  }
+  return (
+    <div>
+      Items:
+      <br />
+      {items}
+      <button onClick="alert(1)">asd</button>
+    </div>
+  );
+}
+
+const element = MyApp();
 OReact.render(element, rootDiv);
 
 // let root = document.getElementById("root")
 
 // ReactDOM.render(App, root);
-
